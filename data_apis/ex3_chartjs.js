eec5727406e0c930f29cfcb11ca24200694719d9
easyas314 <30437725+easyas314@users.noreply.github.com>
2020-05-31 00:53:11 -0400

    chart works, needs some refactoring

--- data_apis/ex3_chartjs.js
+++ data_apis/ex3_chartjs.js
@@ -16,57 +16,48 @@
 
 console.log("CSV charting games");
 
-//var ctx = document.getElementById("framus").getContext('2d');
-const mycanvas = document.getElementById("framus");
-const ctx = mycanvas.getContext('2d');
 var myXlabels = [];
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
+var myYdata = [];
+
+chartIt();
+
+async function chartIt() {
+
+    await getData();
+    console.log('waited out the getData.')
+
+    //var ctx = document.getElementById("framus").getContext('2d');
+    const mycanvas = document.getElementById("framus");
+    //const ctx = mycanvas.getContext('2d');
+    ctx = mycanvas;
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            //labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            labels: myXlabels,
+            datasets: [{
+                //label: '# of Votes',
+                label: 'Global Temperature',
+                //data: [12, 19, 3, 5, 2, 3],
+                data: myYdata,
+                fill: false,
+                //backgroundColor: [ 'rgba(255, 99, 132, 0.2)', ],
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                //borderColor: [ 'rgba(255, 99, 132, 1)', ],
+                borderColor: 'rgba(255, 99, 132, 1)',
+                borderWidth: 1
             }]
-        }
-    }
-});
-
-
-workFile = "test.csv";
+        },
+    });
+}
 
 //getData(workFile);
 
 // async function; async calls w/await keyword
-async function getData(theFile) {
-    const resp = await fetch(theFile);
+async function getData() {
+    const resp = await fetch("ZonAnn.Ts+dSST.csv");
     const data = await resp.text();
-    console.log(data);    
+    //console.log(data);    
     // lots of modules to parse csv files; d3.js p5.js etc
     // ... just doing it ourselves with these methods:
     // ... trim() for trailing empty lines
@@ -80,8 +71,10 @@ async function getData(theFile) {
     table.forEach(row => {
         const columns = row.split(',')
         const year = columns[0];
+        myXlabels.push(year);
         const temp = columns[1];
-        console.log(year, temp)
+        myYdata.push(parseFloat(temp) + 14);
+        console.log(year, temp);
     })
 
 }
